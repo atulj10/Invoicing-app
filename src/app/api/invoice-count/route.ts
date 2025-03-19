ecc2ceee3d269813fc59747c18b8815df42516e1
atulj10 <atulanandj10@gmail.com>
2025-03-19 10:54:35 +0530

    commented unused part

--- src/app/api/invoice-count/route.ts
+++ src/app/api/invoice-count/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import Invoice from "@/models/Invoice";
+// import Invoice from "@/models/Invoice";
 import { connectDB } from "@/lib/db";
 
 export async function GET() {
